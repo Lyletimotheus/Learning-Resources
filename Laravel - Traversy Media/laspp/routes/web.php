24ecc87e7ea836c196c70971a76eda447311d46d
Lyle Timotheus <lyle6494@gmail.com>
2021-05-11 16:29:30 +0200

    Created the controller files

--- Laravel - Traversy Media/laspp/routes/web.php
+++ Laravel - Traversy Media/laspp/routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+/* 
+Get Method followed by the URL and a function referencing the View
+Route::get('/hello', function () {
+    // return view('welcome');
+    return "<h1>Hello World</h1>"; // You can embed HTML into here as well
 });
+
+// Adding dynamic values into the URL
+Route::get('/users/{id}/{name}', function($id, $name) {
+    return 'This is user:' .$name. 'with an id of: ' .$id;
+});
+*/
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+
+Route::get('/about', function() {
+    return view('pages.about');
+});
+
