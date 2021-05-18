0e5f094640a8f79113697dfed8ae57ae0c9e4b56
Lyle Timotheus <lyle6494@gmail.com>
2021-05-18 19:43:51 +0200

    Creating the assets for the project (linking)

--- Laravel/firstproject/app/Http/Controllers/PagesController.php
+++ Laravel/firstproject/app/Http/Controllers/PagesController.php
@@ -11,6 +11,7 @@ class PagesController extends Controller
     }
 
     public function about() {
-        return view('about');
+        $name = "John";
+        return view('about') ->with('name', $name);
     }
 }
