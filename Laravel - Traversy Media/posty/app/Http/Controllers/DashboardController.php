641b78c1939510d5d6876b29ca54d252db947aa5
Lyle Timotheus <lyle6494@gmail.com>
2021-06-28 20:22:19 +0200

    Listing Posts

--- Laravel - Traversy Media/posty/app/Http/Controllers/DashboardController.php
+++ Laravel - Traversy Media/posty/app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -11,7 +12,6 @@ class DashboardController extends Controller
     }
 
     public function index() {
-
         //dd(auth()->user());  Here we check if the user is legit using the user object 
         return view ('dashboard');
     }
--- Laravel - Traversy Media/posty/app/Http/Controllers/PostController.php
+++ Laravel - Traversy Media/posty/app/Http/Controllers/PostController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        $posts = Post::get(); // Return all data in the database in order - returns it as a collection in Laravel
+        return view('posts.index', [
+            'posts' => $posts // Passing down the post to our index views so that we can iterate over each post
+        ]);
     }
 
     public function store(Request $request) {
-        dd('ok');
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create([ //Creating a record in the database
+            'body' => $request->body
+        ]);
+        
+        return back();
     }
 }
