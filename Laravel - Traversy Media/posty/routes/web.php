07f1a3770fe007ff1cdd28e23e18a5c187dce59e
Lyle Timotheus <lyle6494@gmail.com>
2021-07-02 14:02:55 +0200

    Like count eloquent relationship

--- Laravel - Traversy Media/posty/routes/web.php
+++ Laravel - Traversy Media/posty/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserPostController;
 use App\Http\Controllers\PostLikeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
@@ -24,6 +25,8 @@ Route::get('/home', function() {
 Route::get('/dashboard', [DashboardController::class, 'index'])
 ->name('dashboard');
 // ->middleware('auth');
+
+Route::get('/users/{user:username}/posts', [UserPostController::class, 'index'])->name('users.posts');
 
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
