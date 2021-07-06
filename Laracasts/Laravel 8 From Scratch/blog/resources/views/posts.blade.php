082a11896e5c5898a0363506b89d505441cb915f
Lyle Timotheus <lyle6494@gmail.com>
2021-07-06 17:36:26 +0200

    Creating a practice blog

---
+++ Laracasts/Laravel 8 From Scratch/blog/resources/views/posts.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>Post 1</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut aperiam velit eum? Accusantium velit, necessitatibus nihil a neque dolores eaque officia voluptatum accusamus atque cupiditate rerum adipisci itaque soluta hic!</p>
+    </article>
+    <article>
+        <h1>Post 2</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut aperiam velit eum? Accusantium velit, necessitatibus nihil a neque dolores eaque officia voluptatum accusamus atque cupiditate rerum adipisci itaque soluta hic!</p>
+    </article>
+    <article>
+        <h1>Post 3</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut aperiam velit eum? Accusantium velit, necessitatibus nihil a neque dolores eaque officia voluptatum accusamus atque cupiditate rerum adipisci itaque soluta hic!</p>
+    </article>
+</body>
+
