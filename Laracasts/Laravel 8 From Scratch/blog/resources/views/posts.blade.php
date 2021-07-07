3341015f88cfb85a6cd37e04a73033bfdf4bf79d
Lyle Timotheus <lyle6494@gmail.com>
2021-07-07 10:08:20 +0200

    Creating individual post pages

--- Laracasts/Laravel 8 From Scratch/blog/resources/views/posts.blade.php
+++ Laracasts/Laravel 8 From Scratch/blog/resources/views/posts.blade.php
@@ -5,15 +5,15 @@
 
 <body>
     <article>
-        <h1>Post 1</h1>
+        <h1><a href="/posts/my-first-post">Post 1</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut aperiam velit eum? Accusantium velit, necessitatibus nihil a neque dolores eaque officia voluptatum accusamus atque cupiditate rerum adipisci itaque soluta hic!</p>
     </article>
     <article>
-        <h1>Post 2</h1>
+        <h1><a href="/posts/my-second-post">Post 2</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut aperiam velit eum? Accusantium velit, necessitatibus nihil a neque dolores eaque officia voluptatum accusamus atque cupiditate rerum adipisci itaque soluta hic!</p>
     </article>
     <article>
-        <h1>Post 3</h1>
+        <h1><a href="/posts/my-third-post">Post 3</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut aperiam velit eum? Accusantium velit, necessitatibus nihil a neque dolores eaque officia voluptatum accusamus atque cupiditate rerum adipisci itaque soluta hic!</p>
     </article>
 </body>
