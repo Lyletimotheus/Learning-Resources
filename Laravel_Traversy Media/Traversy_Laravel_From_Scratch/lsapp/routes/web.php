0dbf3cd9437b0377008c876ce4b450ae573c8600
Lyle Timotheus <lyle6494@gmail.com>
2021-07-16 23:22:39 +0200

    Setting up the various view pages

--- Laravel_Traversy Media/Traversy_Laravel_From_Scratch/lsapp/routes/web.php
+++ Laravel_Traversy Media/Traversy_Laravel_From_Scratch/lsapp/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+-------------------------
+|Adding a dynamic route
+-------------------------
+Route::get('/users/{id}/{name}', function($id, $name) {
+    return 'This is user '. $name . ' with id '.$id;
 });
+*/
+
+Route::get('/', [PagesController::class, 'index']);
+Route::get('/about', [PagesController::class, 'about']);
+Route::get('/services', [PagesController::class, 'services']);
+
+
+
+
