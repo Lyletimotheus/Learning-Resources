a0cad0002f819596a87215294eda1c1d688dbe18
Lyle Timotheus <lyle6494@gmail.com>
2021-07-16 23:47:47 +0200

    Installing all the front-end dependencies

--- Laravel_Traversy Media/Traversy_Laravel_From_Scratch/lsapp/routes/web.php
+++ Laravel_Traversy Media/Traversy_Laravel_From_Scratch/lsapp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 
@@ -31,3 +32,7 @@ Route::get('/services', [PagesController::class, 'services']);
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
