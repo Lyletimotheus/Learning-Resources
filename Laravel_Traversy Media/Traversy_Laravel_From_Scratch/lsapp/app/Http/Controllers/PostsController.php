1099cd136f882c806b412216ffed5a5b76aca4ab
Lyle Timotheus <lyle6494@gmail.com>
2021-07-20 23:51:28 +0200

    Adding data to the models

--- Laravel_Traversy Media/Traversy_Laravel_From_Scratch/lsapp/app/Http/Controllers/PostsController.php
+++ Laravel_Traversy Media/Traversy_Laravel_From_Scratch/lsapp/app/Http/Controllers/PostsController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    // The following resource methods need to be used
+    // index() -> show list of posts
+    // create() -> create a post
+    // store() -> will submit the post to the DB
+    // edit() -> will edit the post
+    // update() -> the edit function will submit to this function
+    // show() -> show one post
+    // destroy() -> destroy the post
 }
