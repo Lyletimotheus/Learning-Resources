cd9b9f1b1635ffd11ce04559fe3edf5772d2f087
Lyle Timotheus <lyle6494@gmail.com>
2021-07-28 22:36:55 +0200

    Adding the authentication layer to the application

--- Laravel_Traversy Media/Traversy_Laravel_From_Scratch/lsapp/routes/web.php
+++ Laravel_Traversy Media/Traversy_Laravel_From_Scratch/lsapp/routes/web.php
@@ -37,3 +37,7 @@ Route::resource('posts', PostsController::class);
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
